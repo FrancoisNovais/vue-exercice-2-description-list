553f6d7c1333c57744aab0d9f7f0dfc203d50f80
francois6381 <francois.novais@gmail.com>
2024-07-23 23:30:42 +0200

    fav align

--- src/App.vue
+++ src/App.vue
@@ -61,77 +61,88 @@ const fourthPerson = {
     <h1>Description list</h1>
     <div class="cards">
       <div>
-        <img src="./assets/imgs/persons/emily-parker.jpg" alt="" />
-        <h2>{{ firstPerson.firstname }} {{ firstPerson.lastname }}</h2>
-        <p>
-          {{ firstPerson.gender === 'female' ? 'She' : 'He' }} is a {{ firstPerson.age }}-year-old
-          {{ firstPerson.job.toLowerCase() }}.
-        </p>
-        <p>
-          Outsise of work, {{ firstPerson.firstname }} likes to {{ firstPerson.hobbies[0] }} and
-          {{ firstPerson.hobbies[1] }}.
-        </p>
-        <p>
-          At home, {{ firstPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
-          {{ firstPerson.pet.name }} a {{ firstPerson.pet.age }}-years-old
-          {{ firstPerson.pet.species }}.
-        </p>
-        <div class="fav"><font-awesome-icon :icon="['fas', 'heart']" /></div>
+        <div>
+          <img src="./assets/imgs/persons/emily-parker.jpg" alt="" />
+          <h2>{{ firstPerson.firstname }} {{ firstPerson.lastname }}</h2>
+          <p>
+            {{ firstPerson.gender === 'female' ? 'She' : 'He' }} is a {{ firstPerson.age }}-year-old
+            {{ firstPerson.job.toLowerCase() }}.
+          </p>
+          <p>
+            Outsise of work, {{ firstPerson.firstname }} likes to {{ firstPerson.hobbies[0] }} and
+            {{ firstPerson.hobbies[1] }}.
+          </p>
+          <p>
+            At home, {{ firstPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
+            {{ firstPerson.pet.name }} a {{ firstPerson.pet.age }}-years-old
+            {{ firstPerson.pet.species }}.
+          </p>
+        </div>
+
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </div>
 
       <div>
-        <img src="./assets/imgs/persons/marcus-bennett.jpg" alt="" />
-        <h2>{{ secondPerson.firstname }} {{ secondPerson.lastname }}</h2>
-        <p>
-          {{ secondPerson.gender === 'female' ? 'She' : 'He' }} is a {{ secondPerson.age }}-year-old
-          {{ secondPerson.job.toLowerCase() }}.
-        </p>
-        <p>
-          Outsise of work, {{ secondPerson.firstname }} likes to {{ secondPerson.hobbies[0] }} and
-          {{ secondPerson.hobbies[1] }}.
-        </p>
-        <p>
-          At home, {{ secondPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
-          {{ secondPerson.pet.name }} a {{ secondPerson.pet.age }}-years-old
-          {{ secondPerson.pet.species }}.
-        </p>
-        <div class="fav"><font-awesome-icon :icon="['fas', 'heart']" /></div>
+        <div>
+          <img src="./assets/imgs/persons/marcus-bennett.jpg" alt="" />
+          <h2>{{ secondPerson.firstname }} {{ secondPerson.lastname }}</h2>
+          <p>
+            {{ secondPerson.gender === 'female' ? 'She' : 'He' }} is a
+            {{ secondPerson.age }}-year-old {{ secondPerson.job.toLowerCase() }}.
+          </p>
+          <p>
+            Outsise of work, {{ secondPerson.firstname }} likes to {{ secondPerson.hobbies[0] }} and
+            {{ secondPerson.hobbies[1] }}.
+          </p>
+          <p>
+            At home, {{ secondPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
+            {{ secondPerson.pet.name }} a {{ secondPerson.pet.age }}-years-old
+            {{ secondPerson.pet.species }}.
+          </p>
+        </div>
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </div>
       <div>
-        <img src="./assets/imgs/persons/sophia-rodriguez.jpg" alt="" />
-        <h2>{{ thirdPerson.firstname }} {{ thirdPerson.lastname }}</h2>
-        <p>
-          {{ thirdPerson.gender === 'female' ? 'She' : 'He' }} is a {{ thirdPerson.age }}-year-old
-          {{ thirdPerson.job.toLowerCase() }}.
-        </p>
-        <p>
-          Outsise of work, {{ thirdPerson.firstname }} likes to {{ thirdPerson.hobbies[0] }} and
-          {{ thirdPerson.hobbies[1] }}.
-        </p>
-        <p>
-          At home, {{ thirdPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
-          {{ thirdPerson.pet.name }} a {{ thirdPerson.pet.age }}-years-old
-          {{ thirdPerson.pet.species }}..
-        </p>
-        <div class="fav"><font-awesome-icon :icon="['fas', 'heart']" /></div>
+        <div>
+          <img src="./assets/imgs/persons/sophia-rodriguez.jpg" alt="" />
+          <h2>{{ thirdPerson.firstname }} {{ thirdPerson.lastname }}</h2>
+          <p>
+            {{ thirdPerson.gender === 'female' ? 'She' : 'He' }} is a {{ thirdPerson.age }}-year-old
+            {{ thirdPerson.job.toLowerCase() }}.
+          </p>
+          <p>
+            Outsise of work, {{ thirdPerson.firstname }} likes to {{ thirdPerson.hobbies[0] }} and
+            {{ thirdPerson.hobbies[1] }}.
+          </p>
+          <p>
+            At home, {{ thirdPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
+            {{ thirdPerson.pet.name }} a {{ thirdPerson.pet.age }}-years-old
+            {{ thirdPerson.pet.species }}..
+          </p>
+        </div>
+
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </div>
       <div>
-        <img src="./assets/imgs/persons/olivier-hughes.jpg" alt="" />
-        <h2>{{ fourthPerson.firstname }} {{ fourthPerson.lastname }}</h2>
-        <p>
-          {{ fourthPerson.gender === 'female' ? 'She' : 'He' }} is a {{ fourthPerson.age }}-year-old
-          {{ fourthPerson.job.toLowerCase() }}.
-        </p>
-        <p>
-          Outsise of work, {{ fourthPerson.firstname }} likes to {{ fourthPerson.hobbies[0] }} and
-          {{ fourthPerson.hobbies[1] }}.
-        </p>
-        <p>
-          At home, {{ fourthPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
-          {{ fourthPerson.pet.name }} a {{ fourthPerson.pet.age }}-years-old
-          {{ fourthPerson.pet.species }}.
-        </p>
-        <div class="fav"><font-awesome-icon :icon="['fas', 'heart']" /></div>
+        <div>
+          <img src="./assets/imgs/persons/olivier-hughes.jpg" alt="" />
+          <h2>{{ fourthPerson.firstname }} {{ fourthPerson.lastname }}</h2>
+          <p>
+            {{ fourthPerson.gender === 'female' ? 'She' : 'He' }} is a
+            {{ fourthPerson.age }}-year-old {{ fourthPerson.job.toLowerCase() }}.
+          </p>
+          <p>
+            Outsise of work, {{ fourthPerson.firstname }} likes to {{ fourthPerson.hobbies[0] }} and
+            {{ fourthPerson.hobbies[1] }}.
+          </p>
+          <p>
+            At home, {{ fourthPerson.gender === 'female' ? 'her' : 'his' }} lively companion is
+            {{ fourthPerson.pet.name }} a {{ fourthPerson.pet.age }}-years-old
+            {{ fourthPerson.pet.species }}.
+          </p>
+        </div>
+
+        <font-awesome-icon :icon="['fas', 'heart']" />
       </div>
     </div>
   </main>
@@ -155,7 +166,9 @@ main {
   border-radius: 10px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.5);
   width: 280px;
-  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 }
 
 img {
@@ -166,16 +179,13 @@ img {
   object-fit: cover;
   object-position: top;
 }
-.fav {
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-}
-.fav svg {
+
+svg {
   font-size: 24px;
   color: #7f7f7f;
+  align-self: flex-end;
 }
-.fav svg:hover {
+svg:hover {
   font-size: 24px;
   color: #000000;
 }
